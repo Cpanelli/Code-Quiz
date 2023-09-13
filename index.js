06f2aa999df5dd38f39b4e6a4b1a0a982be02fc0
Cpanelli <133393733+Cpanelli@users.noreply.github.com>
2023-09-13 00:25:12 -0400

    filled out my JS somethings seem to be wrong but have a layout of what i want and need

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var timeRemainging = 75000;
+var timeRemaining = 75000; //Time in milliseconds
 var questions = [
   {
     question: "In HTML, what does the ALT attribute in an <img> tag stand for?",
@@ -28,6 +28,7 @@ var questions = [
     correctAnswer: "It returns a value to the caller",
   },
   {
+    // Tried to make this question a littler harder
     question:
       "In JavaScript, what is the purpose of the CONST keyword when declaring a variable?",
     answers: [
@@ -40,3 +41,59 @@ var questions = [
       "To create a variable with a constant value that cannot be reassigned",
   },
 ];
+
+var currentQuestion = 0;
+
+function startQuiz() {
+  //Starting the timer
+  var timer = setInterval(function () {
+    timeRemaining -= 1000;
+    // updating time display
+    document.getElementById("timeRemaining").innerHTML =
+      timeRemaining / 1000 + " seconds remaining";
+    //disable submit button if time runs out.
+    if (timeRemaining <= 0) {
+      document.getElementById("submit").disabled = true;
+    }
+  }, 1000);
+}
+function showQuestion(){
+    var question = questions[currentQuestion];
+    document.getElementById("question") = innerHTML = question.questions;
+//Show answer
+    var answers = question.answers;
+    for (var i =0; i < answers.length; i++);{
+        var answer = document.createElement("input");
+        answer.type = "radio";
+        answer.name = "question" + currentQuestion;
+        answer.value = answers[i];
+        answer.innerHTML = answers[i];
+        document.getElementById("answers").appendChild(answer);
+
+    }
+}
+function submitQuiz(){ //should get users answer
+    var answers = [];
+    for (var i = 0; i < questions.length; i++){
+
+        var answer = document.getElementById("question" + i)[0];
+        answer.push(answer.value);
+    }
+    //see users score
+    var score = 0;
+    for (var i = 0; i < answer.length; i++){
+        if (answers[i] == questions[i].correctAnswer){
+            score++;
+        }
+    }
+    document.getElementById("score").innerHTML = "Your score is" + score + "out of" + questions.length; 
+}
+
+
+window.onload = function(){
+    startQuiz(); //quiz start
+
+    showQuestion(); // shows first question
+
+    document.getElementById("submit").addEventListener("click", submitQuiz);
+}
