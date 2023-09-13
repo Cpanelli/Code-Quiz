e000fffb665c4e0517cdc211cba106d51547ad99
Cpanelli <133393733+Cpanelli@users.noreply.github.com>
2023-09-12 22:22:02 -0400

    added some html add JS...think im doing it right lol

---
+++ index.js
@@ -0,0 +1,42 @@
+var timeRemainging = 75000;
+var questions = [
+  {
+    question: "In HTML, what does the ALT attribute in an <img> tag stand for?",
+    answers: [
+      "Alternate text",
+      "Alignment",
+      " Alternative source",
+      "Animation",
+    ],
+    correctAnswer: "Alternate text",
+  },
+  {
+    question:
+      "Which CSS property is used to control the size of an element's content area, padding, and border combined, without including margins? ",
+    answers: ["Width", "Height", "Margin", "Box-Sizing"],
+    correctAnswer: "Box-Sizing",
+  },
+
+  {
+    question: "What does the RETURN statment do in a JavaScript function? ",
+    answers: [
+      "It displays a message to the user",
+      "It ends the function's execution",
+      "It returns a value to the caller",
+      "It defines a new variable",
+    ],
+    correctAnswer: "It returns a value to the caller",
+  },
+  {
+    question:
+      "In JavaScript, what is the purpose of the CONST keyword when declaring a variable?",
+    answers: [
+      "To declare a variable within a loop",
+      "To create a variable with a constant value that cannot be reassigned",
+      "To define a variable that is only accessible within a specific function",
+      "To declare a variable with global scope",
+    ],
+    answers:
+      "To create a variable with a constant value that cannot be reassigned",
+  },
+];
